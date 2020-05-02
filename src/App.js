cd3a6ccaede56c8cc0e8273708b6d3afa553dd82
malves <malves@verios.com>
2020-05-02 01:16:07 -0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,41 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react';
 
 function App() {
+  const [repositories, setRepositories] = useState([]);
+
+  useEffect(() => {
+    (async function () {
+      const response = await fetch(
+        'https://api.github.com/users/matheus-alvess/repos'
+      );
+      const data = await response.json();
+      setRepositories(data);
+    })();
+  }, []); // ComponentDidMount // será executada apenas uma unica vez.
+
+  useEffect(() => {
+    const filtered = repositories.filter((repo) => repo.favorite);
+    document.title = `Você tem ${filtered.length} favoritos.`;
+  }, [repositories]); // ComponentDidUpdate // será executada quando o estado observado for atualizado.
+
+  function handleFavorite(id) {
+    const newRepositories = repositories.map((repo) => {
+      return repo.id === id ? { ...repo, favorite: !repo.favorite } : repo;
+    });
+
+    setRepositories(newRepositories);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ul>
+      {repositories.map((repo) => (
+        <li key={repo.id}>
+          {repo.name}
+          {repo.favorite && <span>(Favorito)</span>}
+          <button onClick={() => handleFavorite(repo.id)}>Favoritar</button>
+        </li>
+      ))}
+    </ul>
   );
 }
 
